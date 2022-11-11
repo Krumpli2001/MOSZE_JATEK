4d21f5e349422170b0e07156f4fe238821e920c1
Krumpli <sarvari2001@gmail.com>
2022-11-11 11:33:00 +0100

    Story hozzadva

--- Futas.cpp
+++ Futas.cpp
@@ -8,34 +8,83 @@
 
 int kezdes()
 {
-	std::cout << "1. Uj jatek\n2. Mentes folytatasa\n3. Kilepes\n \
-				  Ird be a megfelelo szamot, vagy szoveget a folytatashoz\n";
+	std::cout << "1. Uj jatek\n2. Mentes folytatasa\n3. Costum palya\n4. Kilepes\nIrd be a megfelelo szamot, vagy szoveget a folytatashoz\n";
 	std::string inditas;
 	std::getline(std::cin, inditas);
-	if (isalpha(inditas[0]))
+	bool helyes = false;
+	while (!helyes)
 	{
-		inditas[0] = tolower(inditas[0]);
+		if (isalpha(inditas[0]))
+		{
+			inditas[0] = tolower(inditas[0]);
+		}
+		if (inditas[0] == '4' or inditas[0] == 'k')
+		{
+			/*std::cout << "Beleptel\n";
+			system("pause");*/
+			helyes = true;
+			return 0;
+		}
+		if (inditas[0] == '1' or inditas[0] == 'u')
+		{
+			helyes = true;
+			return 1;
+		}
+		if (inditas[0] == '2' or inditas[0] == 'm')
+		{
+			helyes = true;
+			return 2;
+		}
+		if (inditas[0] == '3' or inditas[0] == 'c')
+		{
+			helyes = true;
+			return 3;
+		}
+		else
+		{
+			std::cout << "Hibas gombot utottel le\n";
+			std::getline(std::cin, inditas);
+		}
 	}
-	if (inditas[0] == '3' or inditas[0] == 'k')
-	{
-		/*std::cout << "Beleptel\n";
-		system("pause");*/
-		return 0;
-	}
-	
-	return 1;
 }
 
-//void run(jatekmenet* g)
-//{
-//	g->kiir( ,'0');
-//	jatekmenet j(4, 5, 3, false, 0, 0, 0, nullptr, false, 0, 0, 0);
-//}
+void story_be(std::string CH)
+{
+	system(CLEAR);
+	std::string egysor;
+
+	std::ifstream f(CH);
+	int karakterek = 0;
+	int sorok = 0;
+
+
+	if (f.is_open())
+	{
+		//palya meretenek meghatarozasa
+		while (std::getline(f, egysor)) {
+			int karakterek = egysor.length();
+			for (int i = 0; i < karakterek; i++) {
+				std::cout << egysor[i];
+				Sleep(5);
+			}
+			std::cout << std::endl;
+		}
+		f.close();
+	}
+	else
+	{
+		std::cerr << "\nNem sikerult a beolvasas\n";
+	}
+	std::cout << std::endl<<"\nNyomj entert a folytatashoz\n";
+	system("pause");
+	system(CLEAR);
+}
 
 void run(int fut)
 {
 	if (fut != 0)
 	{
+		story_be("Story/CH1.txt");
 		jatekmenet j(4, 5, 3, false, 0, 0, 0, nullptr, false, 0, 0, 0);
 		j.beolvas(j, "Text.txt");
 		j.kiir(j, 'M');
--- jatekmenet.hpp
+++ jatekmenet.hpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <stdlib.h>
 
 #include "jatekos.hpp"
 #include "ellenseg.hpp"
