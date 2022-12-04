aac35419faf4a0f9ea2cae382dc23fd7f9459b9d
Krumpli <sarvari2001@gmail.com>
2022-12-04 10:16:25 +0100

    story kerdesek atirasa, hogy ne folyton nyitogasson, zarjon fajlokat

--- Futas.cpp
+++ Futas.cpp
@@ -84,58 +84,6 @@ void story_be(std::string CH)
 }
 
 //rovidebb kerdesekre van, 1-1 sor, "sorkovetes" - nincs press enter a vegen
-void story_kerdesek(std::string CH, int pos)
-{
-	std::string egysor;
-	std::ifstream f(CH);
-	int karakterek = 0;
-
-	if (f.is_open())
-	{
-		f.clear();
-		f.seekg(0);
-		using namespace std::chrono_literals;
-		int hanyadik_sor = 0;
-		while (std::getline(f, egysor)) {
-			int karakterek = egysor.length();
-			bool var = false;
-			//sorok szamolasa
-			if (egysor[0] == '@')
-			{
-				hanyadik_sor++;
-			}
-			//kiiras
-			for (int i = 2; i < karakterek; i++) {
-				// #-nel megall
-				if (egysor[i] == '#' and hanyadik_sor == pos)
-				{
-					var = true;
-				}
-				//@ kiirja a sort
-				if (!var and egysor[0] == '@' and hanyadik_sor == pos)
-				{
-				std::cout << egysor[i];
-				std::this_thread::sleep_for(5ms);
-				}
-			}
-			if (hanyadik_sor == pos)
-			{
-				pos++;
-				std::cout << std::endl;
-			}
-			if (var)
-			{
-				break;
-			}
-		}
-		f.close();
-	}
-	else
-	{
-		std::cerr << "\nNem sikerult a beolvasas\n";
-	}
-}
-
 void story_kerdesek(std::ifstream& f, int pos)
 {
 	std::string egysor;
@@ -248,7 +196,8 @@ void run(int fut)
 			story_be("Story/CH1.txt");
 
 			//kerdes
-			story_kerdesek("Story/CH1_KERDES1.txt", 1);
+			std::ifstream f("Story/CH1_KERDES1.txt");
+			story_kerdesek(f, 1);
 			char elsovetel;
 			char masodikvetel;
 			while (!helyesbe)
@@ -265,52 +214,53 @@ void run(int fut)
 						}
 						else
 						{
-							story_kerdesek("Story/CH1_KERDES1.txt", 15);
+							story_kerdesek(f, 15);
 						}
 					}
 					else
 					{
-						story_kerdesek("Story/CH1_KERDES1.txt", 16);
+						story_kerdesek(f, 16);
 					}
 				}
 				else
 				{
-					story_kerdesek("Story/CH1_KERDES1.txt", 16);
+					story_kerdesek(f, 16);
 				}
 			}
 
 			palya_letrehoz(j, 4, 5, &gelet, false, 0, 0, 0, nullptr, nullptr, 2, 1, 5, 1, 2, 5, 2, 0, 0, 0, "Text.txt", 'M');
 
 			//kerdesre valasz
-			story_kerdesek("Story/CH1_KERDES1.txt", 7);
+			story_kerdesek(f, 7);
 			if (elsovetel == '1' or masodikvetel == '1')
 			{
-				story_kerdesek("Story/CH1_KERDES1.txt", 9);
+				story_kerdesek(f, 9);
 			}
 			else if (elsovetel == '3' or masodikvetel == '3')
 			{
-				story_kerdesek("Story/CH1_KERDES1.txt", 10);
+				story_kerdesek(f, 10);
 			}
 			else if (elsovetel == '4' or masodikvetel == '4')
 			{
-				story_kerdesek("Story/CH1_KERDES1.txt", 11);
+				story_kerdesek(f, 11);
 			}
 			spause();
 
 			//kerdesre valasz
-			story_kerdesek("Story/CH1_KERDES1.txt", 12);
+			story_kerdesek(f, 12);
 			if (elsovetel == '2' or masodikvetel == '2')
 			{
-				story_kerdesek("Story/CH1_KERDES1.txt", 13);
+				story_kerdesek(f, 13);
 				spause();
 			}
 			else
 			{
 				//felteteles labirintus
-				story_kerdesek("Story/CH1_KERDES1.txt", 14);
+				story_kerdesek(f, 14);
 				spause();
 				palya_letrehoz(j, 4, 5, &gelet, false, 0, 0, 0, nullptr, nullptr, 1, 1, 5, 1, 0, 0, 0, 0, 0, 0, "Text.txt", 'M');
 			}
+			f.close();
 			gCP = 1;
 		}
 
@@ -319,7 +269,8 @@ void run(int fut)
 
 			story_be("Story/CH2_1.txt");
 
-			story_kerdesek("Story/CH2_KERDES1.txt", 1);
+			std::ifstream f("Story/CH2_KERDES1.txt");
+			story_kerdesek(f, 1);
 			std::string kardszin;
 			bool helyesbe = false;
 			while (!helyesbe)
@@ -331,7 +282,7 @@ void run(int fut)
 					helyesbe = true;
 				}
 				if (!helyesbe) {
-					story_kerdesek("Story/CH2_KERDES1.txt", 41);
+					story_kerdesek(f, 41);
 				}
 			}
 
@@ -339,7 +290,7 @@ void run(int fut)
 
 			palya_letrehoz(j, 4, 5, &gelet, false, 0, 0, 0, nullptr, nullptr, 1, 1, 5, 1, 0, 0, 0, 0, 0, 0, "Text.txt", 'M');
 
-			story_kerdesek("Story/CH2_KERDES1.txt", 6);
+			story_kerdesek(f, 6);
 
 			char ajtobe;
 			helyesbe = false;
@@ -351,39 +302,39 @@ void run(int fut)
 					helyesbe = true;
 					if (ajtobe == '3')
 					{
-						story_kerdesek("Story/CH2_KERDES1.txt", 11);
+						story_kerdesek(f, 11);
 					}
 				}
 				else
 				{
-					story_kerdesek("Story/CH2_KERDES1.txt", 12);
+					story_kerdesek(f, 12);
 				}
 			}
 			if (ajtobe != '3')
 			{
-				story_kerdesek("Story/CH2_KERDES1.txt", 13);
+				story_kerdesek(f, 13);
 			}
-			story_kerdesek("Story/CH2_KERDES1.txt", 14);
+			story_kerdesek(f, 14);
 			std::string ermemutatas;
 			std::cin >> ermemutatas;
 			ermemutatas[0] = tolower(ermemutatas[0]);
 			if (ermemutatas[0] == 'i')
 			{
-				story_kerdesek("Story/CH2_KERDES1.txt", 19);
+				story_kerdesek(f, 19);
 				germemutatas = true;
 			}
 			if (ajtobe == '3')
 			{
-				story_kerdesek("Story/CH2_KERDES1.txt", 21);
+				story_kerdesek(f, 21);
 			}
 			else
 			{
-				story_kerdesek("Story/CH2_KERDES1.txt", 22);
+				story_kerdesek(f, 22);
 				std::string indok;
 				std::cin >> indok;
-				story_kerdesek("Story/CH2_KERDES1.txt", 23);
+				story_kerdesek(f, 23);
 			}
-			story_kerdesek("Story/CH2_KERDES1.txt", 24);
+			story_kerdesek(f, 24);
 
 			char mitcsinalsz;
 			helyesbe = false;
@@ -392,31 +343,31 @@ void run(int fut)
 				std::cin >> mitcsinalsz;
 				if (mitcsinalsz == '1')
 				{
-					story_kerdesek("Story/CH2_KERDES1.txt", 30);
+					story_kerdesek(f, 30);
 					helyesbe = true;
 				}
 				else if (mitcsinalsz == '2')
 				{
-					story_kerdesek("Story/CH2_KERDES1.txt", 31);
+					story_kerdesek(f, 31);
 				}
 				else if (mitcsinalsz == '3')
 				{
-					story_kerdesek("Story/CH2_KERDES1.txt", 32);
+					story_kerdesek(f, 32);
 				}
 				else if (mitcsinalsz == '4')
 				{
-					story_kerdesek("Story/CH2_KERDES1.txt", 33);
+					story_kerdesek(f, 33);
 				}
 				else
 				{
-					story_kerdesek("Story/CH2_KERDES1.txt", 34);
+					story_kerdesek(f, 34);
 				}
 			}
 
-			story_kerdesek("Story/CH2_KERDES1.txt", 35);
+			story_kerdesek(f, 35);
 			if (ermemutatas[0] == 'i')
 			{
-				story_kerdesek("Story/CH2_KERDES1.txt", 38);
+				story_kerdesek(f, 38);
 			}
 
 			helyesbe = false;
@@ -425,25 +376,26 @@ void run(int fut)
 				std::cin >> mitcsinalsz;
 				if (mitcsinalsz == '1')
 				{
-					story_kerdesek("Story/CH2_KERDES1.txt", 30);
+					story_kerdesek(f, 30);
 					helyesbe = true;
 				}
 				else if (mitcsinalsz == '2')
 				{
-					story_kerdesek("Story/CH2_KERDES1.txt", 39);
+					story_kerdesek(f, 39);
 				}
 				else if ((mitcsinalsz == '3') and (ermemutatas[0] == 'i'))
 				{
-					story_kerdesek("Story/CH2_KERDES1.txt", 40);
+					story_kerdesek(f, 40);
 					helyesbe = true;
 				}
 				else
 				{
-					story_kerdesek("Story/CH2_KERDES1.txt", 34);
+					story_kerdesek(f, 34);
 				}
 			}
-			gCP = 2;
 			spause();
+			f.close();
+			gCP = 2;
 		}
 
 		if (gCP == 2)
@@ -477,7 +429,8 @@ void run(int fut)
 
 			while (gCP == 5)
 			{
-				story_kerdesek("Story/CH6_KERDES1.txt", 1);
+				std::ifstream f("Story/CH6_KERDES1.txt");
+				story_kerdesek(f, 1);
 				bool helyesbe = false;
 				bool ded = false;
 				while (!helyesbe)
@@ -502,15 +455,16 @@ void run(int fut)
 					}
 					else
 					{
-						story_kerdesek("Story/CH6_KERDES1.txt", 2);
+						story_kerdesek(f, 2);
 					}
 				}
 				if (ded)
 				{
-					story_kerdesek("Story/CH6_KERDES1.txt", 3);
+					story_kerdesek(f, 3);
 					spause();
 					gCP = 5;
 				}
+				f.close();
 			}
 		}
 
@@ -519,7 +473,8 @@ void run(int fut)
 
 			story_be("Story/CH7.txt");
 			palya_letrehoz(j, 4, 5, &gelet, false, 0, 0, 0, nullptr, nullptr, 1, 1, 5, 1, 0, 0, 0, 0, 0, 0, "Text.txt", 'M');
-			story_kerdesek("Story/CH7_KERDES1.txt", 1);
+			std::ifstream f("Story/CH7_KERDES1.txt");
+			story_kerdesek(f, 1);
 
 			bool tovabb = false;
 			while (!tovabb)
@@ -528,7 +483,7 @@ void run(int fut)
 				std::cin >> el;
 				if (el == '1')
 				{
-					story_kerdesek("Story/CH7_KERDES1.txt", 6);
+					story_kerdesek(f, 6);
 					char valasztas;
 					std::cin >> valasztas;
 					if (isalpha(valasztas))
@@ -536,14 +491,14 @@ void run(int fut)
 						valasztas = tolower(valasztas);
 						if (valasztas == 'i')
 						{
-							story_kerdesek("Story/CH7_KERDES1.txt", 7);
+							story_kerdesek(f, 7);
 							spause();
 							tovabb = true;
 						}
 						else
 						{
-							story_kerdesek("Story/CH7_KERDES1.txt", 8);
-							story_kerdesek("Story/CH7_KERDES1.txt", 3);
+							story_kerdesek(f, 8);
+							story_kerdesek(f, 3);
 						}
 					}
 				}
@@ -551,21 +506,21 @@ void run(int fut)
 				else if (el == '2')
 				{
 					char valasztas;
-					story_kerdesek("Story/CH7_KERDES1.txt", 9);
+					story_kerdesek(f, 9);
 					std::cin >> valasztas;
 					if (isalpha(valasztas))
 					{
 						valasztas = tolower(valasztas);
 						if (valasztas == 'i')
 						{
-							story_kerdesek("Story/CH7_KERDES1.txt", 11);
+							story_kerdesek(f, 11);
 							spause();
 							tovabb = true;
 						}
 						else
 						{
-							story_kerdesek("Story/CH7_KERDES1.txt", 8);
-							story_kerdesek("Story/CH7_KERDES1.txt", 3);
+							story_kerdesek(f, 8);
+							story_kerdesek(f, 3);
 
 						}
 					}
@@ -574,21 +529,21 @@ void run(int fut)
 				else if (el == '3')
 				{
 					char valasztas;
-					story_kerdesek("Story/CH7_KERDES1.txt", 12);
+					story_kerdesek(f, 12);
 					std::cin >> valasztas;
 					if (isalpha(valasztas))
 					{
 						valasztas = tolower(valasztas);
 						if (valasztas == 'i')
 						{
-							story_kerdesek("Story/CH7_KERDES1.txt", 13);
+							story_kerdesek(f, 13);
 							spause();
 							tovabb = true;
 						}
 						else
 						{
-							story_kerdesek("Story/CH7_KERDES1.txt", 8);
-							story_kerdesek("Story/CH7_KERDES1.txt", 3);
+							story_kerdesek(f, 8);
+							story_kerdesek(f, 3);
 						}
 					}
 				}
@@ -599,12 +554,14 @@ void run(int fut)
 					spause();
 				}
 			}
+			f.close();
 			gCP = 7;
 		}
 
 		if (gCP == 7)
 		{
-			story_kerdesek("Story/CH7_KERDES2.txt", 1);
+			std::ifstream f("Story/CH7_KERDES2.txt");
+			story_kerdesek(f, 1);
 			spause();
 
 			while (gCP == 7)
@@ -612,32 +569,32 @@ void run(int fut)
 				int eredeti_hp = gelet;
 				palya_letrehoz(j, 4, 5, &gelet, false, 2, 5, 5, &sebzes, nullptr, 1, 1, 5, 1, 0, 0, 0, 0, 0, 0, "Text.txt", 'M');
 
-				story_kerdesek("Story/CH7_KERDES2.txt", 3);
+				story_kerdesek(f, 3);
 
 				char mit;
 				std::cin >> mit;
 
 				if (mit == '1')
 				{
-					story_kerdesek("Story/CH7_KERDES2.txt", 7);
+					story_kerdesek(f, 7);
 					spause();
 					gCP = 8;
 				}
 
 				else if (mit == '2')
 				{
-					story_kerdesek("Story/CH7_KERDES2.txt", 8);
+					story_kerdesek(f, 8);
 					spause();
 					gCP = 8;
 				}
 
 				else if (mit == '3')
 				{
-					story_kerdesek("Story/CH7_KERDES2.txt", 9);
+					story_kerdesek(f, 9);
 					gelet = gelet - sebzes;
 					if (gelet < 0)
 					{
-						story_kerdesek("Story/CH7_KERDES2.txt", 10);
+						story_kerdesek(f, 10);
 						spause();
 						gCP = 7;
 						gelet = eredeti_hp;
@@ -650,10 +607,11 @@ void run(int fut)
 				}
 				else
 				{
-					story_kerdesek("Story/CH7_KERDES2.txt", 11);
+					story_kerdesek(f, 11);
 					spause();
 				}
 			}
+			f.close();
 		}
 
 		if (gCP == 8)
@@ -661,22 +619,25 @@ void run(int fut)
 			story_be("Story/CH8.txt");
 			palya_letrehoz(j, 4, 5, &gelet, true, 2, 5, 0, nullptr, nullptr, 1, 1, 5, 1, 0, 0, 0, 0, 0, 0, "Text.txt", 'G');
 
-			story_kerdesek("CH8_KERDES1.txt", 1);
+			std::ifstream f("Story/CH8_KERDES1.txt");
+			story_kerdesek(f, 1);
 			spause();
 
 			palya_letrehoz(j, 4, 5, &gelet, true, 2, 5, 0, nullptr, nullptr, 1, 1, 5, 1, 0, 0, 0, 0, 0, 0, "Text.txt", 'D');
 
-			story_kerdesek("CH8_KERDES1.txt", 2);
+			story_kerdesek(f, 2);
 			spause();
 
+			f.close();
 			gCP = 9;
 		}
 
 		if (gCP == 9)
 		{
-			story_kerdesek("Story/CH9_KERDES1.txt", 1);
+			std::ifstream f("Story/CH9_KERDES1.txt");
+			story_kerdesek(f, 1);
 			spause();
-			story_kerdesek("Story/CH9_KERDES1.txt", 2);
+			story_kerdesek(f, 2);
 			
 			bool tovabb = false;
 			while (!tovabb)
@@ -689,18 +650,18 @@ void run(int fut)
 					mit[0] = tolower(mit[0]);
 					if (mit[0] == 'g')
 					{
-						story_kerdesek("Story/CH9_KERDES1.txt", 5);
+						story_kerdesek(f, 5);
 						spause();
 						gelet += 10;
 						tovabb = true;
 					}
 					if (mit[0] == 'd')
 					{
-						story_kerdesek("Story/CH9_KERDES1.txt", 6);
+						story_kerdesek(f, 6);
 						gelet -= 2;
 						if (gelet < 0)
 						{
-							story_kerdesek("Story/CH9_KERDES1.txt", 7);
+							story_kerdesek(f, 7);
 							spause();
 							gCP = 7;
 							gelet = eredeti_elet;
@@ -714,15 +675,18 @@ void run(int fut)
 				}
 				else
 				{
-					story_kerdesek("Story/CH9_KERDES1.txt", 8);
+					story_kerdesek(f, 8);
 				}
 			}
+			f.close();
 			gCP = 10;
 		}
 
 		if (gCP == 10)
 		{
-			story_kerdesek("Story/CH10_KERDES1.txt", 1);
+			std::ifstream f("Story/CH10_KERDES1.txt");
+			story_kerdesek(f, 1);
+			f.close();
 			spause();
 			palya_letrehoz(j, 4, 5, &gelet, false, 0, 0, 0, nullptr, nullptr, 1, 1, 5, 1, 0, 0, 0, 0, 0, 0, "Text.txt", 'M');
 
@@ -731,14 +695,16 @@ void run(int fut)
 
 		if (gCP == 11)
 		{
-			story_kerdesek("Story/CH11_KERDES1.txt", 1);
+			std::ifstream f("Story/CH11_KERDES1.txt");
+			story_kerdesek(f, 1);
 			spause();
 			palya_letrehoz(j, 4, 5, &gelet, false, 0, 0, 0, nullptr, nullptr, 1, 1, 5, 1, 0, 0, 0, 0, 0, 0, "Text.txt", 'M');
 
-			story_kerdesek("Story/CH11_KERDES1.txt", 3);
+			story_kerdesek(f, 3);
 			spause();
 			palya_letrehoz(j, 4, 5, &gelet, false, 0, 0, 0, nullptr, nullptr, 1, 1, 5, 1, 0, 0, 0, 0, 0, 0, "Text.txt", 'M');
 
+			f.close();
 			gCP = 12;
 		}
 
@@ -746,7 +712,8 @@ void run(int fut)
 		{
 			story_be("Story/CH12.txt");
 			palya_letrehoz(j, 4, 5, &gelet, true, 2, 5, 0, nullptr, nullptr, 1, 1, 5, 1, 0, 0, 0, 0, 0, 0, "Text.txt", ' ');
-			story_kerdesek("Story/CH12_KERDES1.txt", 1);
+			std::ifstream f("Story/CH12_KERDES1.txt");
+			story_kerdesek(f, 1);
 			spause();
 
 			int sarkany_hp = 3;
@@ -758,26 +725,26 @@ void run(int fut)
 
 				if (sarkany_hp == 3)
 				{
-					story_kerdesek("Story/CH12_KERDES1.txt", 4);
+					story_kerdesek(f, 4);
 
 					char mit;
 					std::cin >> mit;
 
 					if (mit == '1')
 					{
-						story_kerdesek("Story/CH12_KERDES1.txt", 9);
+						story_kerdesek(f, 9);
 						sarkany_hp -= 1;
 						spause();
 					}
 
 					else if (mit == '2')
 					{
-						story_kerdesek("Story/CH12_KERDES1.txt", 10);
+						story_kerdesek(f, 10);
 						gelet -= 2;
 						spause();
 						if (gelet < 0)
 						{
-							story_kerdesek("Story/CH12_KERDES1.txt", 11);
+							story_kerdesek(f, 11);
 							spause();
 							gCP = 12;
 						}
@@ -790,12 +757,12 @@ void run(int fut)
 
 					else if (mit == '3')
 					{
-						story_kerdesek("Story/CH12_KERDES1.txt", 12);
+						story_kerdesek(f, 12);
 						gelet -= 5;
 						spause();
 						if (gelet < 0)
 						{
-							story_kerdesek("Story/CH12_KERDES1.txt", 11);
+							story_kerdesek(f, 11);
 							spause();
 							gCP = 12;
 						}
@@ -808,11 +775,11 @@ void run(int fut)
 
 					else if (mit == '4')
 					{
-						story_kerdesek("Story/CH12_KERDES1.txt", 13);
+						story_kerdesek(f, 13);
 						gelet -= 10;
 						if (gelet < 0)
 						{
-							story_kerdesek("Story/CH12_KERDES1.txt", 11);
+							story_kerdesek(f, 11);
 							spause();
 							gCP = 12;
 						}
@@ -824,17 +791,17 @@ void run(int fut)
 					}
 					else
 					{
-						story_kerdesek("Story/CH12_KERDES1.txt", 14);
+						story_kerdesek(f, 14);
 						spause();
 					}
 				}
 
 				else if (sarkany_hp == 2)
 				{
-					story_kerdesek("Story/CH12_KERDES1.txt", 15);
+					story_kerdesek(f, 15);
 					if (germemutatas)
 					{
-						story_kerdesek("Story/CH12_KERDES1.txt", 18);
+						story_kerdesek(f, 18);
 					}
 
 					char mit;
@@ -842,19 +809,19 @@ void run(int fut)
 
 					if (mit == '1')
 					{
-						story_kerdesek("Story/CH12_KERDES1.txt", 19);
+						story_kerdesek(f, 19);
 						sarkany_hp -= 1;
 						spause();
 					}
 
 					else if (mit == '2')
 					{
-						story_kerdesek("Story/CH12_KERDES1.txt", 20);
+						story_kerdesek(f, 20);
 						gelet -= 7;
 						spause();
 						if (gelet < 0)
 						{
-							story_kerdesek("Story/CH12_KERDES1.txt", 11);
+							story_kerdesek(f, 11);
 							spause();
 							gCP = 12;
 						}
@@ -867,14 +834,14 @@ void run(int fut)
 
 					else if (mit == '3' and germemutatas)
 					{
-						story_kerdesek("Story/CH12_KERDES1.txt", 21);
+						story_kerdesek(f, 21);
 						sarkany_hp -= 1;
 						spause();
 					}
 
 					else
 					{
-						story_kerdesek("Story/CH12_KERDES1.txt", 14);
+						story_kerdesek(f, 14);
 						spause();
 					}
 				}
@@ -882,19 +849,19 @@ void run(int fut)
 				else if (sarkany_hp == 1)
 				{
 
-					story_kerdesek("Story/CH12_KERDES1.txt", 22);
+					story_kerdesek(f, 22);
 
 					char mit;
 					std::cin >> mit;
 
 					if (mit == '1')
 					{
-						story_kerdesek("Story/CH12_KERDES1.txt", 27);
+						story_kerdesek(f, 27);
 						gelet -= 5;
 						spause();
 						if (gelet < 0)
 						{
-							story_kerdesek("Story/CH12_KERDES1.txt", 11);
+							story_kerdesek(f, 11);
 							spause();
 							gCP = 12;
 						}
@@ -907,19 +874,19 @@ void run(int fut)
 
 					else if (mit == '2')
 					{
-						story_kerdesek("Story/CH12_KERDES1.txt", 28);
+						story_kerdesek(f, 28);
 						sarkany_hp -= 1;
 						spause();
 					}
 
 					else if (mit == '3')
 					{
-						story_kerdesek("Story/CH12_KERDES1.txt", 29);
+						story_kerdesek(f, 29);
 						gelet -= 12;
 						spause();
 						if (gelet < 0)
 						{
-							story_kerdesek("Story/CH12_KERDES1.txt", 11);
+							story_kerdesek(f, 11);
 							spause();
 							gCP = 12;
 						}
@@ -932,11 +899,11 @@ void run(int fut)
 
 					else if (mit == '4')
 					{
-						story_kerdesek("Story/CH12_KERDES1.txt", 30);
+						story_kerdesek(f, 30);
 						gelet -= 10;
 						if (gelet < 0)
 						{
-							story_kerdesek("Story/CH12_KERDES1.txt", 11);
+							story_kerdesek(f, 11);
 							spause();
 							gCP = 12;
 						}
@@ -948,12 +915,12 @@ void run(int fut)
 					}
 					else
 					{
-						story_kerdesek("Story/CH12_KERDES1.txt", 14);
+						story_kerdesek(f, 14);
 						spause();
 					}
 				}
 			}
-
+			f.close();
 			story_be("Story/CH13.txt");
 		}
 	}
--- futas.hpp
+++ futas.hpp
@@ -6,7 +6,7 @@ int kezdes();
 
 void story_be(std::string CH);
 
-void story_kerdesek(std::string szoveg, int pos);
+void story_kerdesek(std::ifstream& f, int pos);
 
 void palya_letrehoz(jatekmenet* j, int jancsiX, int jancsiY, int* ghp, bool van_e_boss, int bossX, int bossY, int seb, int* gsebzes, char** palya, int mennyi_coll, int coll1X, int coll1Y, int coll1M, int coll2X, int coll2Y, int coll2M, int coll3X, int coll3Y, int coll3M, std::string palya_neve, char boss_char);
 
